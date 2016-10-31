a004875a8838a4bddbab737498428d412815b042
Jos van de Wofshaar <jos.vandewolfshaar@gmail.com>
2016-10-31 10:18:19 +0100

    added common module for common functions

---
+++ common/environments.py
@@ -0,0 +1,86 @@
+from scipy.misc import imresize
+import gym
+import numpy as np
+
+
+def get_env(env, frames_per_state=3):
+    if env in ['Breakout-v0']:
+        return AtariEnvironment(env, frames_per_state)
+    return ClassicControl(env)
+
+
+class ClassicControl(object):
+
+    def __init__(self, env_name):
+        self.env = gym.make(env_name)
+        self.last_observation = self.env.reset()
+
+    def step(self, action):
+        self.last_observation, reward, terminal, info = self.env.step(action)
+        return self.last_observation, reward, terminal, info
+
+    def reset(self):
+        return self.env.reset()
+
+    def state_shape(self):
+        return self.env.observation_space.shape
+
+    def num_actions(self):
+        return self.env.action_space.n
+
+
+class AtariEnvironment(object):
+
+    def __init__(self, env_name, frames_per_state=4, action_repeat=4):
+        self.env = gym.make(env_name)
+        self.last_observation = self.env.reset()
+        self.frames_per_state = frames_per_state
+        self.state = []
+        self.action_repeat = action_repeat
+
+        assert action_repeat > 0
+
+    def _preprocess_observation(self, observation):
+        """
+        This preprocessing step was taken from "Human-level control through deep reinforcement learning"
+        (Mnih et al 2015).
+        :param observation: the raw observation
+        :return: a preprocessed observation
+        """
+        def rgb2gray(rgb):
+            return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
+
+        # Remove Atari artifacts
+        preprocessed_observation = np.maximum(self.last_observation, observation)
+        # Convert to gray scale and resize
+        return imresize(rgb2gray(preprocessed_observation), (84, 84))
+
+    def step(self, action):
+        for _ in range(self.action_repeat):
+            observation, reward, terminal, info = self.env.step(action)
+            preprocessed_observation = self._preprocess_observation(observation)
+
+            if len(self.state) != self.frames_per_state:
+                self.state.append(preprocessed_observation)
+            else:
+                self.state = self.state[1:] + [preprocessed_observation]
+
+            self.last_observation = observation
+
+        return self.state, reward, terminal, info
+
+    def reset(self):
+        self.state = []
+        self.last_observation = self.env.reset()
+        for _ in range(0, self.frames_per_state):
+            self.step(0)
+
+        assert len(self.state) == self.frames_per_state, 'State length: {}'.format(len(self.state))
+
+        return self.state
+
+    def state_shape(self):
+        return (self.frames_per_state, 84, 84)
+
+    def num_actions(self):
+        return self.env.action_space.n
---
+++ common/tensorboard.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import os
+from collections import OrderedDict
+from deeprl.common.logger import logger
+
+
+def get_sub_dirs_from_hyper_parameters(hp):
+    """
+    This function generates the subdir by looking sorting the hyperparameters on keys and joining them with '/'
+    :param hp:  The HyperParameters object
+    :return:    A subdir to log into
+    """
+    dict = OrderedDict(sorted(hp.__dict__.items()))
+    return '/'.join([param + '={}'.format(val) for param, val in dict.items()])
+
+
+def get_writer_new_event(base_path, hyper_parameters):
+    """
+    Returns the new event path for TensorBoard
+    :param base_path:           The base logging path
+    :param hyper_parameters:    A HyperParameters object
+    :return:                    Returns a SummaryWriter object
+    """
+    # Check if base directory exists, if not create it
+    os.makedirs(base_path, exist_ok=True)
+
+    # Check the current directories in there
+    current_dirs = sorted([o for o in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, o))])
+    print(current_dirs)
+    # We're assuming at this point that we do not exceed 1M runs per version
+    if not current_dirs:
+        # If there are no dirs yet, start at 0
+        rundir = 'run000000'
+    else:
+        # Otherwise make a new one by incrementing the count
+        lastdir     = current_dirs[-1]
+        lastrun     = int(lastdir[3:])
+        rundir      = "run%06d" % (lastrun + 1,)
+    fulldir = os.path.join(base_path, rundir, get_sub_dirs_from_hyper_parameters(hyper_parameters))
+
+    logger.info("Writing TensorBoard logs to {}".format(fulldir))
+    return tf.train.SummaryWriter(fulldir, tf.get_default_graph())
+
