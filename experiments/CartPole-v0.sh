55110e6548fd08d35d8359a71bd48a6f1aaaece7
Jos van de Wofshaar <jos.vandewolfshaar@gmail.com>
2016-11-04 20:05:29 +0100

    adjusted params

--- experiments/CartPole-v0.sh
+++ experiments/CartPole-v0.sh
@@ -1,1 +1,2 @@
-python3 a3c.py --env CartPole-v0 --model small_fcn --t_max 1024 --learning_rate 0.05 --input_shape 4 --n_threads 1 --lr_decay 0.1 --rms_epsilon 10e-8 --beta 0.0
+#!/usr/bin/env bash
+python3 a3c.py --env CartPole-v0 --model small_fcn --t_max 1024 --learning_rate 0.05 --input_shape 4 --n_threads 1 --lr_decay 0.25 --rms_epsilon 10e-8 --beta 0.0 --gamma 0.9
