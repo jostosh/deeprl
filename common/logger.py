4fe6a4deaa3d5e5418435b6f036b22b29eba6f09
Jos van de Wofshaar <jos.vandewolfshaar@gmail.com>
2016-11-05 12:43:35 +0100

    moved logger directory setting to logger.py

--- common/logger.py
+++ common/logger.py
@@ -1,9 +1,35 @@
 import logging
+from os.path import expanduser
+import os
+
+VERSION = 'v0.6'
+LOGDIRBASE = "{}/tensorflowlogs/{}".format(expanduser('~'), VERSION)
+
+
+def get_log_dir():
+
+    # Check if base directory exists, if not create it
+    os.makedirs(LOGDIRBASE, exist_ok=True)
+
+    # Check the current directories in there
+    current_dirs = sorted([o for o in os.listdir(LOGDIRBASE) if os.path.isdir(os.path.join(LOGDIRBASE, o))])
+    # We're assuming at this point that we do not exceed 1M runs per version
+    if not current_dirs:
+        # If there are no dirs yet, start at 0
+        rundir = 'run000000'
+    else:
+        # Otherwise make a new one by incrementing the count
+        lastdir     = current_dirs[-1]
+        lastrun     = int(lastdir[3:])
+        rundir      = "run%06d" % (lastrun + 1,)
+    fulldir = os.path.join(LOGDIRBASE, rundir)
+    return fulldir
+
 
 logger = logging.getLogger('test')
 logger.propagate = False
 logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('[%(levelname)s] (%(threadName)-10s) - %(message)s')
+formatter = logging.Formatter('[%(levelname)s] {%(asctime)s} (%(threadName)-10s) - %(message)s')
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 ch.setFormatter(formatter)
--- common/tensorboard.py
+++ common/tensorboard.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import os
 from collections import OrderedDict
-from deeprl.common.logger import logger
+from deeprl.common.logger import logger, get_log_dir
 from tensorflow.core.framework import summary_pb2
 
 
@@ -25,28 +25,15 @@ def sub_dirs_from_hyper_parameters(hp):
     return '/'.join([(param[:5] if len(param) > 5 else param) + '={}'.format(val) if type(val) is not str else val for param, val in dict.items()])
 
 
-def writer_new_event(base_path, hyper_parameters, session):
+def writer_new_event(hyper_parameters, session):
     """
     Returns the new event path for TensorBoard
     :param base_path:           The base logging path
     :param hyper_parameters:    A HyperParameters object
     :return:                    Returns a SummaryWriter object
     """
-    # Check if base directory exists, if not create it
-    os.makedirs(base_path, exist_ok=True)
-
-    # Check the current directories in there
-    current_dirs = sorted([o for o in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, o))])
-    # We're assuming at this point that we do not exceed 1M runs per version
-    if not current_dirs:
-        # If there are no dirs yet, start at 0
-        rundir = 'run000000'
-    else:
-        # Otherwise make a new one by incrementing the count
-        lastdir     = current_dirs[-1]
-        lastrun     = int(lastdir[3:])
-        rundir      = "run%06d" % (lastrun + 1,)
-    fulldir = os.path.join(base_path, rundir) #, sub_dirs_from_hyper_parameters(hyper_parameters))
+    # Get the logging dir
+    fulldir = get_log_dir()
 
     logger.info("Writing TensorBoard logs to {}".format(fulldir))
     return tf.train.SummaryWriter(fulldir, session.graph)
