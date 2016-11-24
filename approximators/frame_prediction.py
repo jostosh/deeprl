69b3e8af1283a2e42a286504d3dcbce6ac3ac2cd
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2016-11-24 12:12:00 +0100

    frame prediction now runs and converges

--- approximators/frame_prediction.py
+++ approximators/frame_prediction.py
@@ -4,6 +4,7 @@ import tflearn
 from deeprl.common.environments import get_env
 from deeprl.common.logger import logger
 import numpy as np
+import pprint
 
 input_index = 0
 action_index = 1
@@ -32,23 +33,25 @@ class FramePrediction(object):
         #self.input = tf.placeholder(tf.float32, self.input_shape)
         net = tf.transpose(self.input, [0, 2, 3, 1])
         conv1 = tflearn.conv_2d(net, 32, 8, 4, activation='relu', padding='valid', weight_decay=0.,
-                              bias_init=tf.constant_initializer(0.1), reuse=self.reuse, scope='Conv1')
+                                reuse=self.reuse, scope='Conv1', restore=False)
         conv2 = tflearn.conv_2d(conv1, 64, 4, 2, activation='relu', padding='valid', weight_decay=0.,
-                              bias_init=tf.constant_initializer(0.1), reuse=self.reuse, scope='Conv2')
+                                reuse=self.reuse, scope='Conv2', restore=False)
         net = tflearn.flatten(conv2)
-        net = tflearn.fully_connected(net, 256, weight_decay=0., reuse=self.reuse, scope='FC1')
+        net = tflearn.fully_connected(net, 256, weight_decay=0., reuse=self.reuse, scope='FC1', restore=False)
 
         return net, conv1, conv2
 
     def decoding_network(self, incoming, conv1, conv2):
         net = tflearn.reshape(incoming, [-1] + conv2.get_shape().as_list()[1:])
         logger.info('Decoding input shape: {}'.format(net.get_shape()))
-        net = tflearn.conv_2d_transpose(net, 32, 2, activation='relu', output_shape=conv1.get_shape().as_list()[1:],
-                                        weight_decay=0., reuse=self.reuse, scope='DeConv1')
+        logger.info("Shape conv1 {}".format(conv1.get_shape().as_list()[1:]))
+        logger.info("Shape conv2 {}".format(conv2.get_shape().as_list()[1:]))
+        net = tflearn.conv_2d_transpose(net, 32, 4, strides=2, activation='relu', output_shape=conv1.get_shape().as_list()[1:],
+                                        weight_decay=0., reuse=self.reuse, scope='DeConv1', restore=False, padding='valid')
         logger.info("input shape {}".format(self.input_shape))
-        net = tflearn.conv_2d_transpose(net, self.input_shape[0], 4, activation='linear',
-                                        output_shape=[84, 84, 4],
-                                        weight_decay=0., reuse=self.reuse, scope='DeConv2')
+        net = tflearn.conv_2d_transpose(net, self.input_shape[0], 8, strides=4, activation='linear',
+                                        output_shape=[84, 84, 4], padding='valid',
+                                        weight_decay=0., reuse=self.reuse, scope='DeConv2', restore=False)
         return net
 
     def build_network(self):
@@ -57,17 +60,18 @@ class FramePrediction(object):
         self.action_input = tf.placeholder(tf.float32, [None])
         action_one_hot = tflearn.one_hot_encoding(self.action_input, self.num_actions)
         action_embedding = tflearn.fully_connected(action_one_hot, 256, weight_decay=0.0, bias=False,
-                                                   reuse=self.reuse, scope='ActionEmbedding')
+                                                   reuse=self.reuse, scope='ActionEmbedding', restore=False)
         encoding = tflearn.fully_connected(actor_critic_head, 256, weight_decay=0.0, bias=False,
-                                           reuse=self.reuse, scope='EncodingEmbedding')
+                                           reuse=self.reuse, scope='EncodingEmbedding', restore=False)
 
         transformation = tflearn.fully_connected(tf.mul(action_embedding, encoding),
                                                  np.prod(conv2.get_shape().as_list()[1:]), weight_decay=0.0,
-                                                 activation='relu', reuse=self.reuse, scope='Transformation')
-        self.output = tf.transpose(self.decoding_network(transformation, conv1, conv2), [0, 3, 1, 2])
+                                                 activation='relu', reuse=self.reuse, scope='Transformation', restore=False)
+        self.output = tf.transpose(self.decoding_network(transformation, conv1, conv2), [0, 3, 1, 2]) + \
+                      tf.stop_gradient(self.input)
 
         self.target = tf.placeholder(tf.float32, [None] + self.input_shape)
-        self.loss = tf.nn.l2_loss(self.target - self.output)
+        self.loss = tf.reduce_mean((self.target - self.output) ** 2) #tf.nn.l2_loss(self.target - self.output)
 
 
 def generate_data(env, size):
@@ -83,9 +87,9 @@ def generate_data(env, size):
         #logger.info("State shape: {}".format(np.asarray(state).shape))
         action = env.env.action_space.sample()
         next_state, reward, terminal, info = env.step(action)
-        E.append([np.copy(state), action, np.copy(next_state)])
+        E.append([state, action, next_state])
         N += 1
-        state = np.copy(next_state)
+        state = next_state
 
         if terminal:
             D += [E]
@@ -105,7 +109,6 @@ def chain_network(net, n, shape, num_actions):
 
 def sample_from_dataset(D, steps, batch_size):
     batch = []
-    logger.info("Next state shape: {}".format(np.asarray(D[-1][-1][2]).shape))
 
     input_batch = []
     action_batch = []
@@ -135,7 +138,7 @@ def transpose_list(l):
     return list(map(list, zip(*l)))
 
 
-def train():
+def train(sess):
     env = get_env('Breakout-v0', output_shape=[84, 84], frames_per_state=4)
 
     input_shape = [None, 4, 84, 84]
@@ -147,37 +150,35 @@ def train():
         {"num_iter": 1000000, "n_steps": 3, "lr": 0.0001, 'batch_size': 8},
         {"num_iter": 1000000, "n_steps": 5, "lr": 0.0001, 'batch_size': 8},
     ]
-    tf.initialize_all_variables()
 
+    chained_nets = chain_network(frame_predictor, max([p['n_steps'] for p in phases]), input_shape[1:], env.num_actions())
 
-    chained_nets = chain_network(frame_predictor, max([p['n_steps'] for p in phases]), input_shape, env.num_actions())
-
-    D_train = generate_data(env, 100)
+    D_train = generate_data(env, 1000000)
     #D_test  = generate_data(env, 100)
 
     writer = tf.train.SummaryWriter(logdir='~/tensorflowlogs/frame_prediction')
+    optimizers = [tf.train.RMSPropOptimizer(phase['lr'], decay=0.95, momentum=0.9) for phase in phases]
+    all_variables = set(tf.all_variables())
 
     tf.merge_all_summaries()
 
-    combined_losses = [tf.add_n([net.loss for net in chained_nets[:phase['n_steps']]]) for phase in phases]
-    for i, phase in enumerate(phases):
+    combined_losses = [tf.div(tf.add_n([net.loss for net in chained_nets[:phase['n_steps']]]), phase['n_steps'])
+                       for phase in phases]
+    train_steps = [o.minimize(combined_loss) for o, combined_loss in zip(optimizers, combined_losses)]
+    sess.run(tf.initialize_all_variables())
+
+    for i, phase, train_step in zip(range(len(phases)), phases, train_steps):
         iter = 0
 
         iter_placeholder = tf.placeholder(tf.int32, iter)
-        combined_loss = combined_losses[0] #= tf.add_n([net.loss for net in chained_nets[:phase['n_steps']]])
+        combined_loss = combined_losses[i]
         loss_summary = tf.scalar_summary('CombinedLoss{}'.format(i), combined_loss)
-        #writer.add_summary(loss_summary)
-        #optimizer = tf.train.RMSPropOptimizer(phase['lr'], decay=.95, momentum=0.9)
-        #train_op = optimizer.minimize(combined_loss)
+        output = chained_nets[phase['n_steps'] - 1].output
+
+        #train_step = optimizer.minimize(combined_loss)
 
         while iter < phase["num_iter"]:
             inputs, actions, targets = sample_from_dataset(D_train, phase['n_steps'], phase['batch_size'])
-
-            #inputs = [sample[input_index] for sample in batch]
-            #actions = [sample[action_index] for sample in batch]
-            #targets = [sample[target_index] for sample in batch]
-
-            logger.info("Input shape {}".format(np.asarray(inputs).shape))
 
             actions = np.asarray(actions).transpose((1, 0))
             targets = np.asarray(targets).transpose((1, 0, 2, 3, 4))
@@ -185,20 +186,20 @@ def train():
 
             fdict = {net.target: target_num for net, target_num in zip(chained_nets, targets)}
             fdict.update({chained_nets[0].input: inputs})
-            logger.info([(k, val.shape) for k, val in fdict.items()])
             fdict.update({net.action_input: action for net, action in zip(chained_nets, actions)})
 
-            with tf.Session() as sess:
-                loss_num, loss_sum = sess.run(
-                    [combined_loss, loss_summary],
-                    feed_dict=fdict
-                )
-                writer.add_summary(loss_sum)
+            loss_num, loss_sum, _ = sess.run(
+                [combined_loss, loss_summary, train_step],
+                feed_dict=fdict
+            )
+            writer.add_summary(loss_sum)
             logger.info("Phase {}, Iter {}, loss {}".format(i, iter, loss_num))
 
             iter += 1
 
 
 if __name__ == "__main__":
-    train()
+    with tf.Session() as sess:
+        train(sess)
+
 
