d84d28a3b9381b02a5bdf1195f177d7bf462af16
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2016-11-27 21:01:07 +0100

    added exclusive and made number of jobs smaller

--- experiments/A3C_distributed_peregrine.sh
+++ experiments/A3C_distributed_peregrine.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --time=00:10:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=24
+#SBATCH --ntasks=4
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=A3C_FF_v07
 #SBATCH --mail-type ALL
@@ -16,11 +16,11 @@ module load tensorflow/0.10.0-foss-2016a-Python-3.5.1
 srun -n 1 python3 mproj/deeprl/rlmethods/a3c_distributed.py --job_name ps --task_index 0 --env CartPole-v0 --log_dir tensorflowlogs/distributed/cartpole/run0001 &
 
 START=0
-END=24
+END=4
 for (( c=$START; c<$END; c++ ))
 do
     echo Starting worker $c
-    srun -n 1 python3 mproj/deeprl/rlmethods/a3c_distributed.py --env CartPole-v0 --job_name worker --task_index $c --model small_fcn --t_max 1024 --learning_rate 0.05 --input_shape 4 --n_threads $END --rms_decay 0.9 --rms_epsilon 1e-2 --beta 0.0001 --gamma 0.9 --log_dir tensorflowlogs/distributed/cartpole/run0001 &
+    srun -n 1 -N 1 --exclusive python3 mproj/deeprl/rlmethods/a3c_distributed.py --env CartPole-v0 --job_name worker --task_index $c --model small_fcn --t_max 1024 --learning_rate 0.05 --input_shape 4 --n_threads $END --rms_decay 0.9 --rms_epsilon 1e-2 --beta 0.0001 --gamma 0.9 --log_dir tensorflowlogs/distributed/cartpole/run0001 &
     sleep 1
 done
 
