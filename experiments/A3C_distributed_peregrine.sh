24c7ee5406d0b6e683b13efba7cb8de56ba94a1a
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2016-12-02 18:13:16 +0100

    now env is also specified in sbatch call

--- experiments/A3C_distributed_peregrine.sh
+++ experiments/A3C_distributed_peregrine.sh
@@ -12,14 +12,14 @@
 
 module load Python/3.5.1-foss-2016a
 LOGS=/home/s2098407/tensorflowlogs/distributed/$1/$2
-srun -n 1 -N 1 --exclusive python3 mproj/deeprl/rlmethods/a3c_distributed.py --job_name ps --task_index 0 --env Breakout-v0 --log_dir $LOGS &
+srun -n 1 -N 1 --exclusive python3 mproj/deeprl/rlmethods/a3c_distributed.py --job_name ps --task_index 0 --env $1 --log_dir $LOGS &
 
 START=0
 END=16
 for (( c=$START; c<$END; c++ ))
 do
     echo Starting worker $c
-    srun -n 1 -N 1 --exclusive python3 mproj/deeprl/rlmethods/a3c_distributed.py --env Breakout-v0 --job_name worker \
+    srun -n 1 -N 1 --exclusive python3 mproj/deeprl/rlmethods/a3c_distributed.py --env $1 --job_name worker \
     --task_index $c --model a3c_lstm --log_dir $LOGS --port0 $3 &
     sleep 1
 done
