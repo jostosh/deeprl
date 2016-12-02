14d0cc6a15e003c9eee0b578ed2527c00ba5a8f7
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2016-12-02 18:08:47 +0100

    changed peregrine file such that port numbers are specified at the moment of calling

--- experiments/A3C_distributed_peregrine.sh
+++ experiments/A3C_distributed_peregrine.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --time=00:10:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=5
-#SBATCH --job-name=A3C_CartPole_distributed
+#SBATCH --ntasks=17
+#SBATCH --job-name=A3C_Distributed
 #SBATCH --mail-type ALL
 #SBATCH --mail-user jos.vandewolfshaar@gmail.com
 #SBATCH --output job-%j.log
@@ -11,15 +11,16 @@
 #SBATCH --partition=short
 
 module load Python/3.5.1-foss-2016a
-LOGS=/home/s2098407/tensorflowlogs/distributed/cartpole/run0002
-srun -n 1 -N 1 --exclusive python3 mproj/deeprl/rlmethods/a3c_distributed.py --job_name ps --task_index 0 --env CartPole-v0 --log_dir $LOGS &
+LOGS=/home/s2098407/tensorflowlogs/distributed/$1/$2
+srun -n 1 -N 1 --exclusive python3 mproj/deeprl/rlmethods/a3c_distributed.py --job_name ps --task_index 0 --env Breakout-v0 --log_dir $LOGS &
 
 START=0
-END=4
+END=16
 for (( c=$START; c<$END; c++ ))
 do
     echo Starting worker $c
-    srun -n 1 -N 1 --exclusive python3 mproj/deeprl/rlmethods/a3c_distributed.py --env CartPole-v0 --job_name worker --task_index $c --model small_fcn --t_max 1024 --learning_rate 0.05 --input_shape 4 --n_threads $END --rms_decay 0.9 --rms_epsilon 1e-2 --beta 0.0001 --gamma 0.9 --log_dir $LOGS &
+    srun -n 1 -N 1 --exclusive python3 mproj/deeprl/rlmethods/a3c_distributed.py --env Breakout-v0 --job_name worker \
+    --task_index $c --model a3c_lstm --log_dir $LOGS --port0 $3 &
     sleep 1
 done
 
