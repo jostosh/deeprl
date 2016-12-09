95600541d778d94fd3d0e22d689497fe821aec53
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2016-12-09 20:54:38 +0100

    updated experiment scripts

--- experiments/A3C_distributed_local.sh
+++ experiments/A3C_distributed_local.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-SLURM_JOB_CPUS_PER_NODE=5 CUDA_VISIBLE_DEVICES='' python3 deeprl/rlmethods/a3c_distributed.py --job_name ps \
+SLURM_JOB_CPUS_PER_NODE=3 CUDA_VISIBLE_DEVICES='' python3 deeprl/rlmethods/a3c_distributed.py --job_name ps \
 --task_index 0 --env CartPole-v0 --log_dir /home/jos/tensorflowlogs/distributed/$1 --port0 $2 &
 
 START=0
-END=4
+END=2
 for (( c=$START; c<$END; c++ ))
 do
     echo Starting worker $c
-    SLURM_JOB_CPUS_PER_NODE=5 CUDA_VISIBLE_DEVICES='' python3 deeprl/rlmethods/a3c_distributed.py --env CartPole-v0 \
+    SLURM_JOB_CPUS_PER_NODE=3 CUDA_VISIBLE_DEVICES='' python3 deeprl/rlmethods/a3c_distributed.py --env CartPole-v0 \
     --job_name worker --task_index $c --model small_fcn --t_max 1024 --learning_rate 0.01 --input_shape 4 --n_threads 4 \
     --rms_decay 0.9 --rms_epsilon 1e-2 --beta 0.0001 --gamma 0.9 --log_dir /home/jos/tensorflowlogs/distributed/$1 --port0 $2 \
     & \
--- experiments/A3C_distributed_local_atari.sh
+++ experiments/A3C_distributed_local_atari.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-SLURM_JOB_CPUS_PER_NODE=5 CUDA_VISIBLE_DEVICES='' python3 deeprl/rlmethods/a3c_distributed.py --job_name ps \
+SLURM_JOB_CPUS_PER_NODE=10 CUDA_VISIBLE_DEVICES='' python3 deeprl/rlmethods/a3c_distributed_backup.py --job_name ps \
 --task_index 0 --env CartPole-v0 --log_dir /home/jos/tensorflowlogs/distributed/$1 --port0 $2 &
 
+SLURM_JOB_CPUS_PER_NODE=10 CUDA_VISIBLE_DEVICES='' python3 deeprl/rlmethods/a3c_distributed_backup.py --job_name ps \
+--task_index 1 --env CartPole-v0 --log_dir /home/jos/tensorflowlogs/distributed/$1 --port0 $2 &
+
+
 START=0
-END=4
+END=8
 for (( c=$START; c<$END; c++ ))
 do
     echo Starting worker $c
-    SLURM_JOB_CPUS_PER_NODE=5 CUDA_VISIBLE_DEVICES='' python3 deeprl/rlmethods/a3c_distributed.py --env Breakout-v0 \
+    SLURM_JOB_CPUS_PER_NODE=10 CUDA_VISIBLE_DEVICES='' python3 deeprl/rlmethods/a3c_distributed_backup.py --env Breakout-v0 \
     --job_name worker --task_index $c --model a3c_ff --log_dir /home/jos/tensorflowlogs/distributed/$1 --port0 $2 \
     & \
 
