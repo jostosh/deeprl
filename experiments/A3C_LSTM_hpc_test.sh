1060471f5f6e7a0c45ec5fe804df1f717bbdfe1e
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-01-06 14:37:59 +0100

    added test script to run on cartesius

---
+++ experiments/A3C_LSTM_hpc_test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --job-name=A3C_LSTM_v09
+#SBATCH --mail-type ALL
+#SBATCH --mail-user jos.vandewolfshaar@gmail.com
+#SBATCH --output job-A3C_LSTM_v09-%j.log
+#SBATCH --mem=2000
+#SBATCH --partition=gpu_short
+
+module load python/3.5.0
+
+srun python3 $HOME/mproj/deeprl/rlmethods/a3c.py --model a3c_lstm --n_threads 16 --clip_rewards $*
