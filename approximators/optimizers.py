be2f5c043d3e7adbed42dd2079613176f0812b42
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-01-07 20:06:05 +0100

    now we use the builtin rms prop properly:)

--- approximators/optimizers.py
+++ approximators/optimizers.py
@@ -1,12 +1,18 @@
 import tensorflow as tf
 from tensorflow.python.framework import constant_op
+from tensorflow.python.training import training_ops
 
 
 class RMSPropCustom(object):
 
-    def __init__(self, session, learning_rate, decay=0.99, epsilon=1e-8, theta=None):
+    def __init__(self, session, learning_rate, decay=0.99, epsilon=1e-8, theta=None, momentum=0.):
         self.learning_rate = learning_rate
         self.decay = decay
+
+        self.decay_tensor = tf.convert_to_tensor(decay, name="DecayRate")
+        self.momentum_tensor = tf.convert_to_tensor(momentum, name='Momentum')
+        self.epsilon_tensor = tf.convert_to_tensor(epsilon, name="Epsilon")
+
         self.epsilon = epsilon
         if theta:
             self.build_update(theta)
@@ -14,6 +20,7 @@ class RMSPropCustom(object):
 
         self.g_moving_average = None
         self.global_theta = None
+        self.mom = None
 
     def build_update(self, theta):
 
@@ -32,22 +39,27 @@ class RMSPropCustom(object):
 
     def _init_from_prototype(self, theta):
         with tf.name_scope("MovingAverageGradient"):
-            self.g_moving_average = [tf.Variable(tf.zeros(shape=var.get_shape().as_list(), dtype=tf.float32)) for var in theta]
+            self.g_moving_average = [tf.Variable(tf.zeros(shape=var.get_shape().as_list(), dtype=tf.float32))
+                                     for var in theta]
+            self.mom = [tf.Variable(tf.zeros(shape=var.get_shape().as_list(), dtype=tf.float32)) for var in theta]
 
     def set_global_theta(self, theta):
         self.global_theta = theta
         self._init_from_prototype(theta)
 
     def build_update_from_vars(self, theta, loss):
-        assert self.global_theta and self.g_moving_average
+        assert self.global_theta and self.g_moving_average and self.mom
 
         with tf.name_scope("GradientInput"):
             grads = [tf.clip_by_norm(grad, 40.0) for grad in tf.gradients(loss, theta)]
 
-        return self.build_update_from_grads(grads)
+        return tf.group(*[training_ops.apply_rms_prop(
+            t, rms, m, self.learning_rate, self.decay_tensor, self.momentum_tensor,
+            self.epsilon_tensor, g, use_locking=False
+        ) for t, rms, m, g in zip(self.global_theta, self.g_moving_average, self.mom, grads)], name="RMSPropUpdate")
+        #return self.build_update_from_grads(grads)
 
     def build_update_from_grads(self, grads):
-        print(type(self.g_moving_average), type(grads))
 
         with tf.name_scope("MovingAverageGradientUpdate"):
             g_update = [tf.assign(g, self.decay * g + (1 - self.decay) * tf.square(d_t), use_locking=False)
