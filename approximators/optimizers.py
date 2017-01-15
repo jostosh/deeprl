3c25b0aef562132c5afadbee43249a8d5aa50b3b
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-01-15 18:14:15 +0100

    removed .op

--- approximators/optimizers.py
+++ approximators/optimizers.py
@@ -54,9 +54,9 @@ class RMSPropCustom(object):
             grads = [tf.clip_by_norm(grad, 40.0) for grad in tf.gradients(loss, theta)]
 
         return tf.group(*[training_ops.apply_rms_prop(
-            t, rms, m, self.learning_rate, self.decay_tensor, self.momentum_tensor,
-            self.epsilon_tensor, g, use_locking=False
-        ).op for t, rms, m, g in zip(self.global_theta, self.g_moving_average, self.mom, grads)], name="RMSPropUpdate")
+            var=t, ms=ms, mom=mom, lr=self.learning_rate, rho=self.decay_tensor, momentum=self.momentum_tensor,
+            epsilon=self.epsilon_tensor, grad=g, use_locking=False
+        ) for t, ms, mom, g in zip(self.global_theta, self.g_moving_average, self.mom, grads)], name="RMSPropUpdate")
         #return self.build_update_from_grads(grads)
 
     def build_update_from_grads(self, grads):
