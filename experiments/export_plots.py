0fbedacf7422843ecc93c853774d22305e4926f8
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-02-04 16:26:39 +0100

    export plots script now groups together data based on env

--- experiments/export_plots.py
+++ experiments/export_plots.py
@@ -18,7 +18,32 @@ rc('text', usetex=True)
 import plotly.offline as py
 import time
 from plotly.graph_objs import *
+import colorlover as cl
 
+colorscale = cl.scales['11']['qual']['Set3']
+
+layout = Layout(
+    paper_bgcolor='rgb(255,255,255)',
+    plot_bgcolor='rgb(229,229,229)',
+    xaxis=XAxis(
+        gridcolor='rgb(255,255,255)',
+        showgrid=True,
+        showline=False,
+        showticklabels=True,
+        tickcolor='rgb(127,127,127)',
+        ticks='outside',
+        zeroline=False
+    ),
+    yaxis=YAxis(
+        gridcolor='rgb(255,255,255)',
+        showgrid=True,
+        showline=False,
+        showticklabels=True,
+        tickcolor='rgb(127,127,127)',
+        ticks='outside',
+        zeroline=False
+    ),
+)
 
 def event_arrays_to_np_arrays(event_array):
     value_by_step = {}
@@ -40,7 +65,7 @@ def event_arrays_to_np_arrays(event_array):
 
 def export_plots():
 
-    event_files_by_hp = {}
+    event_files_by_hp_by_env = {}
     for root, dir, files in os.walk(args.input_dir):
         if any([IsTensorFlowEventsFile(f) for f in files]) and 'hyper_parameters.pkl' in files:
             with open(os.path.join(root, 'hyper_parameters.pkl'), 'rb') as f:
@@ -50,90 +75,73 @@ def export_plots():
             event_files = [os.path.join(root, f) for f in files if IsTensorFlowEventsFile(f)]
             hyper_parameters_str = json.dumps(hyper_parameters, sort_keys=True)
 
-            if hyper_parameters_str not in event_files_by_hp:
-                event_files_by_hp[hyper_parameters_str] = event_files
+            if hyper_parameters['env'] not in event_files_by_hp_by_env:
+                event_files_by_hp_by_env[hyper_parameters['env']] = {hyper_parameters_str: event_files}
+            elif hyper_parameters_str not in event_files_by_hp_by_env[hyper_parameters['env']]:
+                event_files_by_hp_by_env[hyper_parameters['env']][hyper_parameters_str] = event_files
             else:
-                event_files_by_hp[hyper_parameters_str] += event_files
+                event_files_by_hp_by_env[hyper_parameters['env']][hyper_parameters_str] += event_files
 
-    events_by_scalar_by_hp = {}
+    for env, event_files_by_hp in event_files_by_hp_by_env.items():
+        hp_idx = 0
 
-    for hyper_parameters_str, event_files in event_files_by_hp.items():
-        hyper_parameters = json.loads(hyper_parameters_str)
-        events_by_scalar = {}
-        print("Currently looking at {} event files".format(len(event_files)))
-        for event_file in event_files:
-            ea = event_accumulator.EventAccumulator(event_file)
-            ea.Reload()
-            for scalar in ea.Tags()['scalars']:
-                if args.scalar_subset and scalar not in args.scalar_subset:
-                    continue
-                items = ea.Scalars(scalar)
-                if scalar not in events_by_scalar:
-                    events_by_scalar[scalar] = [items]
-                else:
-                    events_by_scalar[scalar].append(items)
+        data_objs = []
+        for hyper_parameters_str, event_files in event_files_by_hp.items():
+            hyper_parameters = json.loads(hyper_parameters_str)
+            events_by_scalar = {}
+            print("Currently looking at {} event files".format(len(event_files)))
+            for event_file in event_files:
+                ea = event_accumulator.EventAccumulator(event_file)
+                ea.Reload()
+                for scalar in ea.Tags()['scalars']:
+                    if args.scalar_subset and scalar not in args.scalar_subset:
+                        continue
+                    items = ea.Scalars(scalar)
+                    if scalar not in events_by_scalar:
+                        events_by_scalar[scalar] = [items]
+                    else:
+                        events_by_scalar[scalar].append(items)
 
-        for scalar, event_arrays in events_by_scalar.items():
-            steps, values, errors = event_arrays_to_np_arrays(event_arrays)
+            for scalar, event_arrays in events_by_scalar.items():
+                steps, values, errors = event_arrays_to_np_arrays(event_arrays)
 
-            steps, values, errors = zip(*sorted(zip(steps, values, errors)))
-            steps = np.asarray(steps)
-            values = np.asarray(values)
-            errors = np.asarray(errors)
+                steps, values, errors = zip(*sorted(zip(steps, values, errors)))
+                steps = np.asarray(steps)
+                values = np.asarray(values)
+                errors = np.asarray(errors)
 
-            print("Now considering {}".format(scalar))
-            pprint.pprint(hyper_parameters)
+                print("Now considering {}".format(scalar))
+                pprint.pprint(hyper_parameters)
 
-            trace1 = Scatter(
-                x=np.concatenate([steps, steps[::-1]]),
-                y=np.concatenate([values+errors, (values-errors)[::-1]]),
-                fill='tozerox',
-                fillcolor='rgba(0,100,80,0.2)',
-                line=Line(color='transparent'),
-                showlegend=False
-            )
-            line1 = Scatter(
-                x=steps,
-                y=values,
-                line=Line(color='rgb(0,100,8)'),
-                mode='lines'
-            )
-            data = Data([trace1, line1])
-            layout = Layout(
-                paper_bgcolor='rgb(255,255,255)',
-                plot_bgcolor='rgb(229,229,229)',
-                xaxis=XAxis(
-                    gridcolor='rgb(255,255,255)',
-                    showgrid=True,
-                    showline=False,
-                    showticklabels=True,
-                    tickcolor='rgb(127,127,127)',
-                    ticks='outside',
-                    zeroline=False
-                ),
-                yaxis=YAxis(
-                    gridcolor='rgb(255,255,255)',
-                    showgrid=True,
-                    showline=False,
-                    showticklabels=True,
-                    tickcolor='rgb(127,127,127)',
-                    ticks='outside',
-                    zeroline=False
-                ),
-            )
-            fig = Figure(data=data, layout=layout)
-            py.plot(fig)
-            time.sleep(5)
+                trace = Scatter(
+                    x=np.concatenate([steps, steps[::-1]]),
+                    y=np.concatenate([values+errors, (values-errors)[::-1]]),
+                    fill='tozerox',
+                    fillcolor=colorscale[hp_idx].replace('rgb', 'rgba').replace(')', ',0.2)'),#'rgba(0,100,80,0.2)',
+                    line=Line(color='transparent'),
+                    showlegend=False
+                )
+                line = Scatter(
+                    x=steps,
+                    y=values,
+                    line=Line(color=colorscale[hp_idx]),
+                    mode='lines'
+                )
+                data_objs += [trace, line]
+                hp_idx += 1
+
+        data = Data(data_objs)
+        fig = Figure(data=data, layout=layout)
+        py.plot(fig)
+        time.sleep(5)
 
 
-            #plt.rc('font', family='serif')
-            #plt.plot(steps, values, 'k-')
-            #plt.fill_between(steps, values - errors, values + errors, facecolor='green')
-            #plt.grid(True, color='w', linestyle='-', linewidth=2)
-            #plt.gca().patch.set_facecolor('0.8')
-            #plt.show()
-
-
+                #plt.rc('font', family='serif')
+                #plt.plot(steps, values, 'k-')
+                #plt.fill_between(steps, values - errors, values + errors, facecolor='green')
+                #plt.grid(True, color='w', linestyle='-', linewidth=2)
+                #plt.gca().patch.set_facecolor('0.8')
+                #plt.show()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
