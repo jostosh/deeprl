51b84d7b9fa299d5ab1525c29c1432a6330968c3
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-02-11 10:57:22 +0100

    changed default configuration for test script for peregrine

--- experiments/A3C_LSTM_peregrine_test.sh
+++ experiments/A3C_LSTM_peregrine_test.sh
@@ -3,13 +3,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
-#SBATCH --job-name=A3C_LSTM_v09
+#SBATCH --job-name=A3C_FF_TEST_v09
 #SBATCH --mail-type ALL
 #SBATCH --mail-user jos.vandewolfshaar@gmail.com
-#SBATCH --output job-A3C_LSTM_v09-%j.log
+#SBATCH --output job-A3C_FF_TEST_v09-%j.log
 #SBATCH --mem=2000
 #SBATCH --partition=short
 
 module load Python/3.5.1-foss-2016a
 
-srun python mproj/deeprl/rlmethods/a3c.py --model a3c_lstm --n_threads 12 --clip_rewards $*
+srun python mproj/deeprl/rlmethods/a3c.py --model a3c_ff --n_threads 12 --clip_rewards $* --env Catch --evaluation_interval 50000
