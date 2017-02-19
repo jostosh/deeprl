74f7ca1e60ad3344565d5ba3281876873689db95
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-02-19 19:27:39 +0100

    added option for passing a3c commands

--- experiments/param_sweep.py
+++ experiments/param_sweep.py
@@ -24,11 +24,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", default='a3c_ff_ss')
     parser.add_argument("--params", nargs='+', default=['learning_rate'])
+    parser.add_argument("--a3c_args", nargs='+', default=[])
     args = parser.parse_args()
+
+    a3c_args = ['--' + a for a in args.a3c_args]
 
     for i in range(50):
         command = ["python3", "mproj/deeprl/rlmethods/a3c.py", '--env=Catch', '--model={}'.format(args.model),
-                   '--evaluation_interval=50000', '--T_max=1000000',  '--n_threads=12'] +\
+                   '--evaluation_interval=50000', '--T_max=1000000',  '--n_threads=12'] + a3c_args +\
                   ["--{}={}".format(p, log_uniform(lo_by_param[p], hi_by_param[p], np.random.rand()))
                    for p in args.params]
         print('INITIATING COMMAND:\n{}'.format(' '.join(command)))
