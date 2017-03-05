fixed minor issue with spatial softmax weights

approximators/layers.py
@@ -201,7 +201,7 @@ def spatialsoftmax(incoming, epsilon=0.01, trainable_temperature=True, name='Spa
         # Concatenate the resulting tensors to get the output
         o = tf.concat(1, [x_coordinates, y_coordinates] + ([x_coordinates_s, y_coordinates_s] if hierarchical else []),
                       name="Output")
-        o.b = temperature if not hierarchical else tf.concat(0, [temperature, temperature_patch])
+        o.b = [temperature] if not hierarchical else [temperature, temperature_patch]
         tf.add_to_collection(tf.GraphKeys.LAYER_VARIABLES + '/' + name, o.b)
         o.sm = softmax_per_channel
 

approximators/nn.py
@@ -136,7 +136,7 @@ class ActorCriticNN(object):
             net = conv_layer(net, 64, 3, 2, activation=tf.identity, name='Conv2')
             self._add_trainable(net)
             net = spatialsoftmax(net, epsilon=self.hp.ss_epsilon)
-            self.theta.append(net.b)
+            self.theta += net.b
             net = fc_layer(net, 256, activation=self.hp.activation, name='FC3')
             self._add_trainable(net)
             self.embedding_layer = net