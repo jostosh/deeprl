6ec1d165ca278b5c1a273d37c124f88b77ae2f3d
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-03-08 21:54:29 +0100

    fixed adam optimizer and added optional bias correction for rmsprop and direct clipping of feedback coefficients

--- approximators/optimizers.py
+++ approximators/optimizers.py
@@ -7,7 +7,8 @@ import numpy as np
 class RMSPropShared(object):
 
     def __init__(self, session, learning_rate, decay=0.99, epsilon=1e-8, theta=None, momentum=0., feedback=False,
-                 thl=0.5, thu=2., feedback_decay=0.99, global_clipping=False, global_clip_norm=1.0):
+                 thl=0.5, thu=2., feedback_decay=0.99, global_clipping=False, global_clip_norm=1.0, d_clip_lo=0.1,
+                 d_clip_hi=10, ms_bias_correction=False):
         self.learning_rate = learning_rate
         self.decay = decay
 
@@ -24,35 +25,21 @@ class RMSPropShared(object):
         self.global_theta = None
         self.mom = None
 
-        self.thl = tf.Variable(thl, dtype=tf.float32, trainable=False)
-        self.thu = tf.Variable(thu, dtype=tf.float32, trainable=False)
+        self.thl = thl #tf.Variable(thl, dtype=tf.float32, trainable=False)
+        self.thu = thu # tf.Variable(thu, dtype=tf.float32, trainable=False)
         self.d = tf.Variable(1., dtype=tf.float32, trainable=False)
 
         self.loss_prev = tf.Variable(0., dtype=tf.float32, trainable=False)
         self.feedback = feedback
-        self.t = tf.Variable(0, dtype=tf.int32, trainable=False)
+        self.t = tf.Variable(0.0, dtype=tf.float32, trainable=False)
         self.feedback_decay = feedback_decay
         self.updates = []
         self.global_clipping = global_clipping
         self.global_clip_norm = global_clip_norm
+        self.d_clip_lo = d_clip_lo
+        self.d_clip_hi = d_clip_hi
+        self.ms_bias_correction = ms_bias_correction
 
-
-    '''
-    def build_update(self, theta):
-
-        with tf.name_scope("GradientInput"):
-            self.gradients = [tf.placeholder(tf.float32, shape=var.get_shape().as_list()) for var in theta]
-
-        with tf.name_scope("MovingAverageGradient"):
-            self.ms = [tf.Variable(tf.zeros(shape=var.get_shape().as_list(), dtype=tf.float32))
-                                for var in theta]
-            self.g_update = [tf.assign(g, self.decay * g + (1 - self.decay) * tf.square(d_t), use_locking=False)
-                             for g, d_t in zip(self.ms, self.gradients)]
-        with tf.name_scope("RMSPropMinimize"):
-            self.minimize = tf.group(*[tf.assign_add(t, -self.learning_rate * tf.div(grad, tf.sqrt(g_mov_avg + self.epsilon)),
-                                                     use_locking=False) for
-                                       t, grad, g_mov_avg in zip(theta, self.gradients, self.g_update)])
-    '''
 
     def _init_from_prototype(self, theta):
         with tf.name_scope("MovingAverageGradient"):
@@ -73,31 +60,32 @@ class RMSPropShared(object):
             else:
                 grads = [tf.clip_by_norm(grad, 40.0) for grad in tf.gradients(loss, theta)]
 
-        #return tf.group(*[training_ops.apply_rms_prop(
-        #    var=t, ms=ms, mom=mom, lr=self.learning_rate, rho=self.decay_tensor, momentum=self.momentum_tensor,
-        #    epsilon=self.epsilon_tensor, grad=g, use_locking=False
-        #) for t, ms, mom, g in zip(self.global_theta, self.g_moving_average, self.mom, grads)], name="RMSPropUpdate")
-        other_updates = []
+        other_updates = [tf.assign_add(self.t, 1, use_locking=False, name="Increment_t")]
         d_t = None
         if self.feedback:
             not_first_iter = tf.greater(self.t, 1)
+            loss_prev = tf.Variable(0., dtype=tf.float32, trainable=False)
+            loss_hat_prev = tf.Variable(0., dtype=tf.float32, trainable=False)
 
-            new_loss_greater = tf.greater(loss, self.loss_prev)
-            ch_fact_lbound = tf.cond(new_loss_greater, lambda: 1 + self.thl, lambda: 1/(1 + self.thu))
-            ch_fact_ubound = tf.cond(new_loss_greater, lambda: 1 + self.thu, lambda: 1/(1 + self.thl))
+            cond = tf.greater(loss, loss_prev)
+            ch_fact_lbound = tf.cond(cond, lambda: 1 + self.thl, lambda: 1 / (1 + self.thu))
+            ch_fact_ubound = tf.cond(cond, lambda: 1 + self.thu, lambda: 1 / (1 + self.thl))
+            loss_ch_fact = loss / loss_prev
+            loss_ch_fact = tf.maximum(loss_ch_fact, ch_fact_lbound)
+            loss_ch_fact = tf.minimum(loss_ch_fact, ch_fact_ubound)
+            loss_hat = tf.cond(not_first_iter, lambda: loss_hat_prev * loss_ch_fact, lambda: loss)
 
-            loss_ch_fact = loss / self.loss_prev
-            loss_ch_fact = tf.maximum(ch_fact_lbound, loss_ch_fact)#tf.cond(tf.less(loss_ch_fact, ch_fact_lbound), lambda: ch_fact_lbound, lambda: loss_ch_fact)
-            loss_ch_fact = tf.minimum(ch_fact_ubound, loss_ch_fact) #tf.cond(tf.greater(loss_ch_fact, ch_fact_ubound), lambda: ch_fact_ubound, lambda: loss_ch_fact)
-            loss_hat = tf.cond(not_first_iter, lambda: self.loss_prev * loss_ch_fact, lambda: loss)
-
-            d_den = tf.minimum(loss_hat, self.loss_prev) # tf.cond(tf.greater(loss_hat, self.loss_prev), lambda: self.loss_prev, lambda: loss_hat)
-            d_t = self.feedback_decay * self.d + (1.-self.feedback_decay) * tf.abs((loss_hat - self.loss_prev) / d_den)
-            d_t = tf.cond(not_first_iter, lambda: d_t, lambda: tf.constant(1.))
+            d_den = tf.minimum(loss_hat, loss_hat_prev)  # tf.cond(tf.greater(loss_hat, loss_prev), )
+            d_t = (self.feedback_decay * self.d) + (1. - self.feedback_decay) * tf.abs((loss_hat - loss_hat_prev) / d_den)
+            d_t = tf.clip_by_value(
+                tf.cond(not_first_iter, lambda: d_t, lambda: tf.constant(1.)),
+                self.d_clip_lo,
+                self.d_clip_hi
+            )
 
             other_updates = [tf.assign(self.d, d_t, use_locking=False),
-                             tf.assign_add(self.t, 1, use_locking=False, name="Increment_t"),
-                             tf.assign(self.loss_prev, loss_hat, use_locking=False)]
+                             tf.assign(self.loss_prev, loss, use_locking=False),
+                             tf.assign(loss_hat_prev, loss_hat)]
 
         return self.build_update_from_grads(grads, other_updates=other_updates, d_t=d_t)
 
@@ -107,7 +95,9 @@ class RMSPropShared(object):
             g_update = [tf.assign(ms, self.decay * ms + (1 - self.decay) * tf.square(grad), use_locking=False)
                         for ms, grad in zip(self.ms, grads)]
         with tf.name_scope("RMSPropMinimize"):
-            minimize = tf.group(*([tf.assign_add(t, -self.learning_rate *
+            lr_t = self.learning_rate if not self.ms_bias_correction \
+                else self.learning_rate * (1 - tf.pow(self.decay, self.t+1))
+            minimize = tf.group(*([tf.assign_add(t, -lr_t *
                                       tf.div(grad, tf.sqrt(ms + self.epsilon) * (d_t if self.feedback else 1.)),
                                       use_locking=False)
                                    for t, grad, ms in zip(self.global_theta, grads, g_update)] + other_updates),
@@ -117,8 +107,9 @@ class RMSPropShared(object):
 
 class AdamShared(object):
 
-    def __init__(self, session, learning_rate, epsilon=1e-8, theta=None, feedback=False,
-                 thl=0.5, thu=2., feedback_decay=0.99, beta1=0.9, beta2=0.999):
+    def __init__(self, session, learning_rate, epsilon=1e-8, theta=None, feedback=False, global_clipping=False,
+                 thl=0.1, thu=10., feedback_decay=0.99, beta1=0.9, beta2=0.999, d_clip_lo=0.2, d_clip_hi=5,
+                 global_clip_norm=1.0):
         self.beta1 = tf.convert_to_tensor(beta1)
         self.beta2 = tf.convert_to_tensor(beta2)
         self.learning_rate = learning_rate
@@ -137,9 +128,13 @@ class AdamShared(object):
 
         self.loss_prev = tf.Variable(0., dtype=tf.float32, trainable=False)
         self.feedback = feedback
-        self.t = tf.Variable(0, dtype=tf.int32, trainable=False)
+        self.t = tf.Variable(0.0, dtype=tf.float32, trainable=False)
         self.feedback_decay = feedback_decay
         self.updates = []
+        self.d_clip_lo = d_clip_lo
+        self.d_clip_hi = d_clip_hi
+        self.global_clipping = global_clipping
+        self.global_clip_norm = global_clip_norm
 
     def _init_from_prototype(self, theta):
         with tf.name_scope("MovingAverageGradient"):
@@ -163,36 +158,44 @@ class AdamShared(object):
         assert self.global_theta and self.ms and self.vs
 
         with tf.name_scope("GradientInput"):
-            grads = [self._clip_grad(grad) for grad in tf.gradients(loss, theta)]
+            if self.global_clipping:
+                grads, _ = tf.clip_by_global_norm(tf.gradients(loss, theta), self.global_clip_norm)
+            else:
+                grads = [tf.clip_by_norm(grad, 40.0) for grad in tf.gradients(loss, theta)]
 
-
-
-
-        other_updates = []
+        other_updates = [tf.assign_add(self.t, 1, use_locking=False, name="Increment_t")]
         d_t = None
         if self.feedback:
-            not_first_iter = tf.greater(self.t, 1)
+            not_first_iter = tf.greater(self.t, 1.0)
+            loss_prev = tf.Variable(0., dtype=tf.float32, trainable=False)
+            loss_hat_prev = tf.Variable(0., dtype=tf.float32, trainable=False)
 
-            new_loss_greater = tf.greater(loss, self.loss_prev)
-            ch_fact_lbound = tf.cond(new_loss_greater, lambda: 1 + self.thl, lambda: 1/(1 + self.thu))
-            ch_fact_ubound = tf.cond(new_loss_greater, lambda: 1 + self.thu, lambda: 1/(1 + self.thl))
+            cond = tf.greater(loss, loss_prev)
+            ch_fact_lbound = tf.cond(cond, lambda: 1 + self.thl, lambda: 1 / (1 + self.thu))
+            ch_fact_ubound = tf.cond(cond, lambda: 1 + self.thu, lambda: 1 / (1 + self.thl))
+            loss_ch_fact = loss / loss_prev
+            loss_ch_fact = tf.maximum(loss_ch_fact, ch_fact_lbound)
+            loss_ch_fact = tf.minimum(loss_ch_fact, ch_fact_ubound)
+            loss_hat = tf.cond(not_first_iter, lambda: loss_hat_prev * loss_ch_fact, lambda: loss)
 
-            loss_ch_fact = loss / self.loss_prev
-            loss_ch_fact = tf.maximum(ch_fact_lbound, loss_ch_fact)#tf.cond(tf.less(loss_ch_fact, ch_fact_lbound), lambda: ch_fact_lbound, lambda: loss_ch_fact)
-            loss_ch_fact = tf.minimum(ch_fact_ubound, loss_ch_fact) #tf.cond(tf.greater(loss_ch_fact, ch_fact_ubound), lambda: ch_fact_ubound, lambda: loss_ch_fact)
-            loss_hat = tf.cond(not_first_iter, lambda: self.loss_prev * loss_ch_fact, lambda: loss)
-
-            d_den = tf.minimum(loss_hat, self.loss_prev) # tf.cond(tf.greater(loss_hat, self.loss_prev), lambda: self.loss_prev, lambda: loss_hat)
-            d_t = self.feedback_decay * self.d + (1.-self.feedback_decay) * tf.abs((loss_hat - self.loss_prev) / d_den)
-            d_t = tf.cond(not_first_iter, lambda: d_t, lambda: tf.constant(1.))
+            d_den = tf.minimum(loss_hat, loss_hat_prev)  # tf.cond(tf.greater(loss_hat, loss_prev), )
+            d_t = (self.feedback_decay * self.d) + (1. - self.feedback_decay) * tf.abs((loss_hat - loss_hat_prev) / d_den)
+            d_t = tf.clip_by_value(
+                tf.cond(not_first_iter, lambda: d_t, lambda: tf.constant(1.)),
+                self.d_clip_lo,
+                self.d_clip_hi
+            )
 
             other_updates = [tf.assign(self.d, d_t, use_locking=False),
-                             tf.assign_add(self.t, 1, use_locking=False, name="Increment_t"),
-                             tf.assign(self.loss_prev, loss_hat, use_locking=False)]
+                             tf.assign(self.loss_prev, loss, use_locking=False),
+                             tf.assign(loss_hat_prev, loss_hat)]
 
         return self.build_update_from_grads(grads, other_updates=other_updates, d_t=d_t)
 
     def build_update_from_grads(self, grads, other_updates, d_t):
+
+        lr_t = self.learning_rate * (tf.sqrt(1. - tf.pow(self.beta2, self.t+1)) /
+                                     (1. - tf.pow(self.beta1, self.t+1)))
 
         with tf.name_scope("GradientMomentUpdate"):
             m_t = [tf.assign(m, (self.beta1 * m) + (1. - self.beta1) * g)            for m, g in zip(self.ms, grads)]
@@ -200,7 +203,7 @@ class AdamShared(object):
         with tf.name_scope("RMSPropMinimize"):
             minimize = tf.group(
                 *([
-                    tf.assign_add(p, -self.learning_rate * m / (tf.sqrt(v) + self.epsilon))
+                    tf.assign_add(p, -lr_t * m / (tf.sqrt(v) * (d_t if d_t is not None else 1) + self.epsilon))
                     for p, m, v in zip(self.global_theta, m_t, v_t)
                 ] + other_updates)
             )
