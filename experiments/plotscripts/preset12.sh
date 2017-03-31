f9e999c20414bd6146f0b901bb637554e66a7dad
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-03-31 22:07:41 +0200

    minor changes in plotscript for preset12

--- experiments/plotscripts/preset12.sh
+++ experiments/plotscripts/preset12.sh
@@ -5,7 +5,7 @@ python3 export_plots.py \
     --trace_by learning_rate global_clip_norm \
     --image_suffix sweep_preset12 \
     --log_scale \
-    --title "Parameter sweep with Eve on Catch" \
-    --yrange 0 1 \
-    --ylabel "Mean score final 10 evals" \
-    --xlabel "$\log_{10}(\alpha)$"
+    --title "Global clip norm sweep" \
+    --ylabel "$\log_{10}(c)$" \
+    --xlabel "$\log_{10}(\alpha)$" \
+    --display
