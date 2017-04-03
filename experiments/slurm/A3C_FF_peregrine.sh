4de527bad6962e4208c4281296be6c4fdb1052ca
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-04-03 09:55:17 +0200

    reduced requested time for a3c ff

--- experiments/slurm/A3C_FF_peregrine.sh
+++ experiments/slurm/A3C_FF_peregrine.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#SBATCH --time=96:00:00
+#SBATCH --time=72:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
-#SBATCH --job-name=A3C_FF
+#SBATCH --job-name=A3C_FF_DEFAULT
 #SBATCH --mail-type ALL
 #SBATCH --mail-user jos.vandewolfshaar@gmail.com
 #SBATCH --output job-A3C_FF_DEFAULT-%j.log
