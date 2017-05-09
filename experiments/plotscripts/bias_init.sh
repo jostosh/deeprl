c66a6372e56a72290821431cdc027bfa78227030
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-05-09 11:41:22 +0200

    updated plotscripts

--- experiments/plotscripts/bias_init.sh
+++ experiments/plotscripts/bias_init.sh
@@ -2,16 +2,16 @@
 LOGBASE=~/tensorflowlogs/peregrine/v0.9.5/sweep
 
 python3 export_plots.py \
-    --input_dir $LOGBASE/preset13 $LOGBASE/preset42 \
+    --input_dir $LOGBASE/preset42 $LOGBASE/preset55 \
     --mode sweep \
     --trace_by learning_rate \
-    --image_suffix sweep_multiple_test \
+    --image_suffix bias_init \
     --log_scale \
     --xrange -6 -2 \
     --yrange 0 1 \
-    --title "Parameter initializations" \
+    --title "Bias intialization" \
     --xlabel "$\log_{10}(\alpha)$" \
     --ylabel "Mean score" \
-    --labels "A3C FF torch" "A3C FF"
+    --labels "Torch" "$\xi = 0.01$" \
     --legend_at "upper left" \
     $*
--- experiments/plotscripts/policy_quantization.sh
+++ experiments/plotscripts/policy_quantization.sh
@@ -2,7 +2,7 @@
 LOGBASE=~/tensorflowlogs/peregrine/v0.9.5/sweep
 
 python3 export_plots.py \
-    --input_dir $LOGBASE/preset13 $LOGBASE/preset47 $LOGBASE/preset48 $LOGBASE/preset49 $LOGBASE/preset50 \
+    --input_dir $LOGBASE/preset55 $LOGBASE/preset56 $LOGBASE/preset48 $LOGBASE/preset49 $LOGBASE/preset50 \
     --mode sweep \
     --trace_by learning_rate \
     --image_suffix policy_quantization2 \
