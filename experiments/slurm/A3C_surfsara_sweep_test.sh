48e54f62b9481b5222d07d51b686f465c340fb37
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-05-21 10:01:30 +0200

    added new test script for surfsara sweep

---
+++ experiments/slurm/A3C_surfsara_sweep_test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#SBATCH --time=48:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+#SBATCH --job-name=SWEEP
+#SBATCH --mail-type ALL
+#SBATCH --mail-user jos.vandewolfshaar@gmail.com
+#SBATCH --output job-SWEEP-%j.log
+#SBATCH --mem=2000
+#SBATCH --partition short
+
+
+module load python/3.5.0
+module load cuda/7.5.18
+module load cudnn/7.5-v5
+module load gcc/4.9.2
+
+export PYTHONPATH="$PYTHONPATH:/home/jvdw/mproj"
+
+srun python mproj/deeprl/experiments/param_sweep.py $*
