1526f9ace36caeb66d2a071ff6026173f3bb1542
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-05-29 20:02:25 +0200

    increased cpu count

--- experiments/slurm/A3C_LSTM_surfsara_sweep.sh
+++ experiments/slurm/A3C_LSTM_surfsara_sweep.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=24
 #SBATCH --job-name=SWEEP
 #SBATCH --mail-type ALL
 #SBATCH --mail-user jos.vandewolfshaar@gmail.com
