f6139f8ee46823fd1f7623e6ff388a34e0378e21
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-06-17 10:32:13 +0200

    added a3c ww script for surfsara

---
+++ experiments/slurm/surfsara/A3C_WW.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#SBATCH --time=36:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --job-name=A3C_SS
+#SBATCH --mail-type ALL
+#SBATCH --mail-user jos.vandewolfshaar@gmail.com
+#SBATCH --output job-SS-%j.log
+#SBATCH --mem=2000
+
+module load python/3.5.0
+module load cuda/7.5.18
+module load cudnn/7.5-v5
+module load gcc/4.9.2
+
+export PYTHONPATH="$PYTHONPATH:/home/jvdw/mproj"
+
+srun python3 $HOME/mproj/deeprl/rlmethods/a3c.py --model a3c_ff_ww --ss_temp 1.0  --n_threads 12 $*
