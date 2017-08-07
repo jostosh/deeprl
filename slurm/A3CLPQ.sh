a2d0763debb139d35d01a2bd342d14816a9e0729
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-08-07 20:22:23 +0200

    added lpq experiment

---
+++ slurm/A3CLPQ.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#SBATCH --time=40:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --job-name=A3C_GLPQ
+#SBATCH --mail-type ALL
+#SBATCH --mail-user jos.vandewolfshaar@gmail.com
+#SBATCH --output job-GLPQ-%j.log
+#SBATCH --mem=2000
+
+module load tensorflow
+
+source $HOME/envs/mproj10/bin/activate
+
+srun python3 $HOME/mproj/deeprl/train.py \
+    --model a3clpq \
+    --n_threads 16 \
+    --entropy_beta 0.001 \
+    $*
