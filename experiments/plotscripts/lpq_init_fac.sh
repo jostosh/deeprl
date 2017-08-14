63e488989aea391076a289093cb0ab33b4618c2e
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2017-08-14 21:48:08 +0200

    changed plotscript

--- experiments/plotscripts/lpq_init_fac.sh
+++ experiments/plotscripts/lpq_init_fac.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
-LOGBASE=~/tensorflowlogs/peregrine/v0.9.5/sweep
-
 python3 export_plots.py \
-    --input_dir $LOGBASE/preset141 $LOGBASE/preset142 \
+    --input_dir ~/tensorflowlogs/peregrine/v0.9.5/sweep/preset143 \
     --mode sweep \
-    --trace_by lpq_init_fac \
-    --image_suffix policy_quantization_best \
+    --trace_by learning_rate lpq_init_fac \
+    --image_suffix sweep_preset12 \
     --log_scale \
-    --xrange -1.204 1.204 \
-    --yrange 0 1 \
-    --title "Temperature Sweep LPQ" \
-    --xlabel "$\log_{10}(\rho_{LPQ})$" \
-    --ylabel "Mean score" \
-    --labels "A3C random uniform" "A3C zero clip"  \
-    --legend_at "upper left" \
-    $*
+    --title "LPQ init bounds" \
+    --ylabel "$\log_{10}(\rho_{LPQ})$" \
+    --xlabel "$\log_{10}(\eta)$" \
+    --fontsize 20 \
+    --display
